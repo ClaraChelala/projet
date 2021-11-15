04f8a79958362085170c96fe75d0b3f6773a27a2
ClaraChelala <83409958+ClaraChelala@users.noreply.github.com>
2021-11-15 11:35:37 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,42 +1,47 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { createStore, combineReducers } from 'redux';
-import {Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 
 import './App.css';
 
 import AuthRoute from './components/auth/AuthRoute';
-import Home from './pages/Home';
-import AdminPosts from './pages/AdminPosts';
+import UserTopics from './pages/UserTopics';
 import Posts from './pages/Posts';
-import Signin from './pages/Signin'
-import Users from './pages/Users'
-import NewTopic from './pages/NewTopic'
+import Signin from './pages/Signin';
+import Users from './pages/Users';
+import NewTopic from './pages/NewTopic';
+import UserPosts from './pages/UserPosts';
+import NewUserPost from './pages/NewUserPost';
 
 import Navbar from './components/layout/Navbar';
 
 import postsReducer from './store/reducers/posts';
 import usersReducer from './store/reducers/users';
-
+import TopicsReducer from './store/reducers/topics';
+import AdminTopics from './pages/AdminTopics';
 
 const rootReducer = combineReducers({
-  posts : postsReducer,
+  posts: postsReducer,
   users: usersReducer,
+  topics : TopicsReducer,
 });
 
 const store = createStore(rootReducer);
 
 function App() {
   return (
-    <Provider store={store}>
+    <Provider store={store}> 
       <Router>
         <Navbar />
         <Route exact path="/signin" component={Signin} />
-        <AuthRoute exact path="/Home" component={Home} />
         <AuthRoute exact path="/Users" component={Users} />
-        <AuthRoute exact path="/AdminTopics" component={AdminPosts} />
+        <AuthRoute exact path="/AdminTopics" component={AdminTopics} />
+        <AuthRoute exact path="/UserTopics" component={UserTopics} />
         <AuthRoute exact path="/Posts" component={Posts} />
         <AuthRoute exact path="/NewTopic" component={NewTopic} />
+        <AuthRoute exact path="/UserPosts" component={UserPosts} />
+        <AuthRoute exact path="/NewUserPost" component={NewUserPost} />
       </Router>
     </Provider>
   );
