a47d3b60f0f45d7602032e2f22d74acc3cca1079
ClaraChelala <83409958+ClaraChelala@users.noreply.github.com>
2021-11-15 11:38:09 +0200

    Add files via upload

--- src/components/auth/AuthRoute.js
+++ src/components/auth/AuthRoute.js
@@ -1,1 +1,18 @@
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
 
+function AuthRoute ({ component: Component, ...restOfProps }) {
+  const isAuthenticated = localStorage.getItem("isAuthenticated");
+  // console.log("this", isAuthenticated);
+
+  return (
+    <Route
+      {...restOfProps}
+      render={(props) =>
+        isAuthenticated ? <Component {...props} /> : <Redirect to="/signin" />
+      }
+    />
+  );
+}
+
+export default AuthRoute;
